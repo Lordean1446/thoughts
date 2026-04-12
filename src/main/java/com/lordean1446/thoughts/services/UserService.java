e6a8e5c303faba9ec5378ca6fe78aec8c5778103
Lordean1446 <edleandrosouza@gmail.com>
2026-04-12 19:16:51 -0300

    add exception handling for user not found and implement findById method in UserService and UserResource

--- src/main/java/com/lordean1446/thoughts/services/UserService.java
+++ src/main/java/com/lordean1446/thoughts/services/UserService.java
@@ -2,6 +2,7 @@ package com.lordean1446.thoughts.services;
 
 import com.lordean1446.thoughts.domain.User;
 import com.lordean1446.thoughts.repository.UserRepository;
+import com.lordean1446.thoughts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
