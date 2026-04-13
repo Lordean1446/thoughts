ccfcfee12e3408df503e53bfa17b9e7667bae04b
Lordean1446 <edleandrosouza@gmail.com>
2026-04-12 22:18:15 -0300

    add insert method in UserService and implement POST endpoint in UserResource for user creation

--- src/main/java/com/lordean1446/thoughts/services/UserService.java
+++ src/main/java/com/lordean1446/thoughts/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lordean1446.thoughts.services;
 
 import com.lordean1446.thoughts.domain.User;
+import com.lordean1446.thoughts.dto.UserDTO;
 import com.lordean1446.thoughts.repository.UserRepository;
 import com.lordean1446.thoughts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,13 @@ public class UserService {
 
     public User findById(String id) {
         return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
